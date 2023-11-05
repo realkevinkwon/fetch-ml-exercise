4130e5bb575ba2c1b782d48ecd8539eb8be50f6e
realkevinkwon <kevinkwonmail@gmail.com>
2023-11-05 08:27:42 -0500

    more stuff but i'm gonna pivot to some other method

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
-def data_visualization(receipt_data):
-    fig, ax = plt.subplots()
+def data_visualization(receipt_df):
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'b', 'w']
+    for i in range(52):
+        plt.plot(receipt_df['day_of_week'][i*7+3:i*7+10], receipt_df['count'][i*7+3:i*7+10], color=colors[i%8])
 
     plt.show()
 
@@ -13,20 +15,19 @@ def preprocess_data(receipt_df):
         'year': [],
         'month': [],
         'day': [],
+        'week': [],
         'day_of_week': [],
         'count': receipt_df['Receipt_Count']
     }
 
     for row in date_col:
         year, month, day = row.split('-')
+        date = datetime.date(int(year), int(month), int(day))
         new_df['year'].append(year)
         new_df['month'].append(month)
         new_df['day'].append(day)
-        new_df['day_of_week'].append(
-            datetime.date.weekday(
-                datetime.date(int(year), int(month), int(day))
-            )
-        )
+        new_df['week'].append(date.isocalendar()[1])
+        new_df['day_of_week'].append(date.isoweekday())
 
     return pd.DataFrame(data=new_df)
 
